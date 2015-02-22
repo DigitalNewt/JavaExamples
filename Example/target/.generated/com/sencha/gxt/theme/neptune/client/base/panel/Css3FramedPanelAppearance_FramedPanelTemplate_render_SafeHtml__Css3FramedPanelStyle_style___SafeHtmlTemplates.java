5a547be9bbd458f1b06a585868e8c6df3e988f21
Brent Baker <digitalnewt@me.com>
2015-02-22 15:17:12 -0500

    Delete Example App

--- Example/target/.generated/com/sencha/gxt/theme/neptune/client/base/panel/Css3FramedPanelAppearance_FramedPanelTemplate_render_SafeHtml__Css3FramedPanelStyle_style___SafeHtmlTemplates.java
+++
@@ -1,9 +0,0 @@
-package com.sencha.gxt.theme.neptune.client.base.panel;
-
-import com.google.gwt.safehtml.shared.SafeHtml;
-import com.google.gwt.safehtml.client.SafeHtmlTemplates.Template;
-
-public interface Css3FramedPanelAppearance_FramedPanelTemplate_render_SafeHtml__Css3FramedPanelStyle_style___SafeHtmlTemplates extends com.google.gwt.safehtml.client.SafeHtmlTemplates {
-  @Template("<div class=\"{0}\"><div class=\"{1}\"></div><div class=\"{2}\"><div class=\"{3}\"></div><div class=\"{4}\"></div></div></div>")
-  SafeHtml render0(java.lang.String arg0, java.lang.String arg1, java.lang.String arg2, java.lang.String arg3, java.lang.String arg4);
-}
