2754e8606c53c93b5d863a6113245f6028b2e6ba
Brent Baker <digitalnewt@me.com>
2015-03-30 19:35:08 -0400

    move design patterns

--- Design Patterns/AbstractFactory/src/main/java/EnemyShipFactory.java
+++
@@ -1,9 +0,0 @@
-/**
- * User: Brent Baker
- * Date: 10/22/13
- * Time: 2:22 PM
- */
-public interface EnemyShipFactory {
-    public ESWeapon addESGun();
-    public ESEngine addESEngine();
-}
--- Design Patterns/Observer/src/main/java/Observer.java
+++
@@ -1,9 +0,0 @@
-/**
- * User: Brent Baker
- * Date: 10/15/13
- * Time: 9:12 AM
- * Observer interface
- */
-public interface Observer {
-    public void update(double ibmPrice, double aaplPrice, double googPrice);
-}
